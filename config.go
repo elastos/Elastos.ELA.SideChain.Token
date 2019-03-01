3de4b2f09a14fbfa773b9368835b88faa5ebc288
AlexPan <ppgod@live.cn>
2019-03-01 16:45:12 +0800

    Move ELA.Utility dependency to ELA/utils

--- config.go
+++ config.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/elastos/Elastos.ELA.SideChain.Token/params"
 
-	"github.com/elastos/Elastos.ELA.Utility/elalog"
 	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/utils/elalog"
 )
 
 const (
