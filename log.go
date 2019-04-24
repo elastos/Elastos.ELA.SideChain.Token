4a8a49482f9de270e9d5575039d6f6e594e0261f
AlexPan <ppgod@live.cn>
2019-04-24 20:25:58 +0800

    Simplify configuration parameters

--- log.go
+++ log.go
@@ -13,38 +13,36 @@ import (
 	"github.com/elastos/Elastos.ELA.SideChain/server"
 	"github.com/elastos/Elastos.ELA.SideChain/service"
 	"github.com/elastos/Elastos.ELA.SideChain/spv"
-
 	"github.com/elastos/Elastos.ELA/p2p/addrmgr"
 	"github.com/elastos/Elastos.ELA/p2p/connmgr"
 	"github.com/elastos/Elastos.ELA/utils/elalog"
 )
 
 const (
-	defaultMaxPerLogFileSize int64 = elalog.MBSize * 20
-	defaultMaxLogsFolderSize int64 = elalog.GBSize * 2
+	defaultPerLogFileSize int64 = elalog.MBSize * 20
+	defaultLogsFolderSize int64 = elalog.GBSize * 2
 )
 
 // configFileWriter returns the configured parameters for log file writer.
 func configFileWriter() (string, int64, int64) {
-	maxPerLogFileSize := defaultMaxPerLogFileSize
-	maxLogsFolderSize := defaultMaxLogsFolderSize
-	if cfg.MaxPerLogFileSize > 0 {
-		maxPerLogFileSize = cfg.MaxPerLogFileSize * elalog.MBSize
+	maxPerLogFileSize := defaultPerLogFileSize
+	maxLogsFolderSize := defaultLogsFolderSize
+	if cfg.PerLogFileSize > 0 {
+		maxPerLogFileSize = cfg.PerLogFileSize * elalog.MBSize
 	}
-	if cfg.MaxLogsFolderSize > 0 {
-		maxLogsFolderSize = cfg.MaxLogsFolderSize * elalog.MBSize
+	if cfg.LogsFolderSize > 0 {
+		maxLogsFolderSize = cfg.LogsFolderSize * elalog.MBSize
 	}
 	return filepath.Join(DataPath, defaultLogDir), maxPerLogFileSize, maxLogsFolderSize
 }
 
-// log is a logger that is initialized with no output filters.  This
-// means the package will not perform any logging by default until the caller
-// requests it.
+// log is a logger that is initialized with no output filters.  This means the
+// package will not perform any logging by default until the caller requests it.
 var (
 	fileWriter = elalog.NewFileWriter(configFileWriter())
 	logWriter  = io.MultiWriter(os.Stdout, fileWriter)
 	backend    = elalog.NewBackend(logWriter, elalog.Llongfile)
-	level, _   = elalog.LevelFromString(cfg.LogLevel)
+	level, _   = elalog.LevelFromString(cfg.DebugLevel)
 
 	admrlog = backend.Logger("ADMR", elalog.LevelOff)
 	cmgrlog = backend.Logger("CMGR", elalog.LevelOff)
